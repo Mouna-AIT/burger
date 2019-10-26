8dc79b99974c52ba17d8e0565d47088470ba0b61
Moumix <mouna.aitichou@gmail.com>
2019-10-25 22:53:45 -0400

    added packages,folders,and dependencies

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+use burgers_db;
+INSERT INTO burgers(burger_name)
+VALUES
+    ('Good Burger'),
+    ('SQL Burger'),
+    ('Mamas Burger'),
+    ('Cheese Burger'),
+    ('Kitkat Burger'),
+    ('Sweet Burger'),
+    ('Good Burger');
