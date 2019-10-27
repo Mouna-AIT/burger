f732f446afb4f451e0b5cb19ef0be5e1801b3de7
Moumix <mouna.aitichou@gmail.com>
2019-10-26 23:07:25 -0400

    model setup

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+const orm = require('../config/orm.js');
+
+var burger = {
+    all: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res)
+        })
+    },
+    create: function(name, eaten, cb) {
+        orm.insertOne("burgers", name, eaten, function(res) {
+            cb(res)
+        })
+    },
+    update: function(id, val, cb) {
+        orm.updateOne("burgers", id, val, function(res) {
+            cb(res)
+        })
+    }
+}
+
+
+module.exports = burger
