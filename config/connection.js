21540fc07cfdbed6406bb832b7d8ba0fea65d084
Moumix <mouna.aitichou@gmail.com>
2019-10-27 22:58:11 -0400

    deploying

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,15 @@ const mysql = require('mysql')
 var connection;
 
 if (process.env.JAWSDB_URL) {
+    console.log(process.env.JAWSDB_URL)
     connection = mysql.createConnection(process.env.JAWSDB_URL)
+
 } else {
     connection = mysql.createConnection({
         host: 'etdq12exrvdjisg6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
         user: 'a26yf41badmx186q',
         password: 's42x8lrwbeizv3cr',
-        database: 'burgers_db'
+        database: 'w1idqdwc353236ru'
     })
 }
 
