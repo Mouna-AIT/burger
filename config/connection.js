f72fe49b577664e98c51ae72c2814164389df991
Moumix <mouna.aitichou@gmail.com>
2019-10-27 22:34:04 -0400

    changing host for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
+        host: 'etdq12exrvdjisg6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'a26yf41badmx186q',
+        password: 's42x8lrwbeizv3cr',
         database: 'burgers_db'
     })
 }
